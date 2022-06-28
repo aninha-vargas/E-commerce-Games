2494a013ce42443f6e04719c0f492bf0a813d64d
Ana Maria Vargas <aninha_vargas@hotmail.com>
2022-06-27 21:38:59 -0300

    versao final

--- front/src/app/components/views/home/carrinho/carrinho.component.ts
+++ front/src/app/components/views/home/carrinho/carrinho.component.ts
@@ -1,3 +1,6 @@
+import { Pedido } from './../../../../../models/pedido.model';
+import { CarrinhoService } from 'src/services/carrinho.service';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { ItemVenda } from 'src/models/item-venda';
 
@@ -12,14 +15,37 @@ export class CarrinhoComponent implements OnInit {
   colunasExibidas: String[] = ["imagem", "nome", "quantidade", "preco"];
   valorTotal!: number;
 
-  constructor() { }
+  pedido : ItemVenda;
+  constructor(private service: CarrinhoService,
+        private router: Router) {
+          this.pedido = {} as ItemVenda;
+        }
 
   ngOnInit(): void {
     this.itens = JSON.parse(localStorage.getItem('itens')!)  || [];
     this.valorTotal = this.itens.reduce((total, item) => {
       return total + item.preco * item.quantidade;
   }, 0);
-      console.table(this.itens);
+      // console.table(this.itens);
+      // console.log(this.pedido);
+
+  }
+  // salvar(pedido: ItemVenda) {
+  salvar() {
+    const itens = JSON.parse(localStorage.getItem('itens')!)  || [];
+    this.service.incluirPedido(itens).subscribe(() => {
+      console.log("pedido salvo com sucesso!");
+
+    });
+    localStorage.clear();
+    this.router.navigate(['/home/carrinho/sucesso']);
+  }
+  limpar() {
+    localStorage.clear();
+    window.location.reload();
+  }
+  voltar(){
+    this.router.navigate(['/']);
 
   }
 
